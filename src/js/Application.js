a41c2db6b331f647a1afbf5edb97cd44802f7fc6
Stanislava Pavlova <stanislavap2002@gmail.com>
2022-12-16 19:29:35 +0200

    task

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,25 +1,37 @@
-import EventEmitter from "eventemitter3";
-import Beat from "./Beat";
+import EventEmitter from 'eventemitter3';
+import Beat from './Beat';
 
 export default class Application extends EventEmitter {
   static get events() {
     return {
-      READY: "ready",
+      READY: 'ready',
     };
   }
 
   constructor() {
     super();
 
-    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+    const lyrics = ['Ah', 'ha', 'ha', 'ha', "stayin' alive", "stayin' alive"];
     let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
-
-    document.querySelector(".main").appendChild(message);
+    this._beat = new Beat();
 
     this.emit(Application.events.READY);
+
+    this._beat.addEventListener(Beat.events.BIT, () => {
+      this._create(lyrics[count]);
+      count++;
+      if (count === lyrics.length) {
+        count = 0;
+      }
+    });
+
+  }
+
+  _create(l) {
+    const message = document.createElement('div');
+    message.classList.add('message');
+    message.innerText = l;
+    document.querySelector('.main').appendChild(message);
   }
 }
--- src/js/Beat.js
+++ src/js/Beat.js
@@ -1,13 +1,15 @@
-export default class Beat {
+import EventEmitter from "eventemitter3";
+
+export default class Beat extends EventEmitter {
   static get events() {
     return {
-      BIT: "bit",
+      BIT: 'bit',
     };
   }
 
   constructor() {
     setInterval(() => {
-      console.log("bit");
+      this.emit(Beat.events.BIT);
     }, 600);
   }
 }
